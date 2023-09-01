99f4e2747f04872a5693442c411bf5da196d0525
SILVAKARI <silvaitelemakari@gmail.com>
2023-09-01 07:55:31 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function scrollTOElement(elementSelector, instance = 0) {
+  // Select all elements that match the given selector
+  const elements = document.querySelectorAll(elementSelector);
+  // Check if there are elements matching the selector and if the requested instance exist
+  if (elements.length > instance) {
+    // Scroll to the specified instance of the element
+    elements[instance].scrollIntoView({ behavior: "smooth" });
+  }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener("click", () => {
+  scrollTOElement(".header");
+});
+
+link2.addEventListener("click", () => {
+  // Scroll to the second element with "header2" class
+  scrollTOElement(".header", 1);
+});
+
+link3.addEventListener("click", () => {
+  scrollTOElement(".column");
+});
